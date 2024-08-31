096c17c0bb687cbc4812eb21b3f08be9481ae9e4
May <mayraort95@icloud.com>
2024-08-31 15:16:42 +0200

    Tailwind and Router installed, login and register pages created.

--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,26 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
-import App from './App.jsx'
+import {
+  createBrowserRouter,
+  RouterProvider,
+} from "react-router-dom";
 import './index.css'
+import LoginPage from './pages/Login.page.jsx';
+import RegisterPage from './pages/Register.page.jsx';
+
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <LoginPage />
+  },
+  {
+    path: "/Register",
+    element: <RegisterPage />
+  },
+]);
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
-    <App />
+    <RouterProvider router={router} />
   </StrictMode>,
 )

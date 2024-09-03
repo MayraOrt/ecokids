3b5c34594a69f4573910ac59797eca7372dc6b97
May <mayraort95@icloud.com>
2024-09-03 18:27:31 +0200

    Dashboard finished.

--- src/pages/Dashboard.page.jsx
+++ src/pages/Dashboard.page.jsx
@@ -1,18 +1,26 @@
 
-
 function DashboardPage(){
     return(
-    <div className="main-container flex flex-col items-center">
-        <h1 className="w-full text-center pt-10">Lasst uns lernen</h1>
+    <div className="main-container bg-contain bg-center flex flex-col items-center min-h-screen" style={{ backgroundImage: 'url(/src/assets/fondo.jpg)' }}>
+        <h1 className="text-4xl font-bold text-red-500 w-full text-center pt-16">Lasst uns lernen!</h1>
         <div className="max-w-6xl grid grid-cols-3 gap-20 pt-14">
-            <div className="min-h-20 border-2 border-amber-800 rounded-xl p-3">
-                <p>Was bedeutet Nachhaltigkeit?</p>
+            <div className="min-h-80 border-4 border-lime-500 bg-[#F3F2F0] shadow-md rounded-xl p-8 w-80 justify-between items-center">
+                <div className="text-center mb-4 flex flex-col gap-8 justify-between items-center">
+                    <p className="font-sans text-2xl font-bold">Was bedeutet Nachhaltigkeit?</p>
+                    <img src="/src/assets/hoja.png" alt="Hoja" className="w-1/2 h-auto object-cover rounded-lg"/>
+                </div>
             </div>
-            <div className="min-h-20 border-2 border-pink-400 rounded-xl p-3">
-                <p>Strom sparen leicht gemacht</p>
+            <div className="min-h-80 border-4 border-rose-600 bg-[#F3F2F0] shadow-md rounded-xl p-8 w-80 flex justify-between items-center">
+                <div className="text-center mb-4 flex flex-col gap-8 justify-between items-center">
+                    <p className="font-sans text-2xl font-bold">Deutsch <br /> Freu(n)de</p>
+                    <img src="/src/assets/bandera.png" alt="Flagge" className="w-1/2 h-auto object-cover rounded-lg" />
+                </div>
             </div>
-            <div className="min-h-20 border-2 border-sky-300 rounded-xl p-3">
-                <p>Das Abenteuer der Mülltrennung</p>
+            <div className="min-h-80 border-4 border-blue-400 bg-[#F3F2F0] shadow-md rounded-xl p-8 w-80 flex justify-between items-center">
+                <div className="text-center mb-4 flex flex-col gap-8 justify-between items-center">
+                    <p className="font-sans text-2xl font-bold">Auf ein Mathe-Abenteuer!</p>
+                    <img src="/src/assets/math.png" alt="Math" className="w-1/2 h-auto object-cover rounded-lg"/>
+                </div>
             </div>
         </div>
     </div>
